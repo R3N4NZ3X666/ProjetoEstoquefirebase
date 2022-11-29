a9a01dc1e17653ceec51dfbd8f8e12a3124f63d4
R3N4NZ3X666 <111936435+R3N4NZ3X666@users.noreply.github.com>
2022-11-28 21:33:26 -0300

    Update authentication.js

--- js/authentication.js
+++ js/authentication.js
@@ -1,7 +1,7 @@
 //baseURL
 const baseURL = window.location.hostname.includes('127.0.0.1')
   ? 'http://127.0.0.1:5500'
-  : 'https://fatecitu-pi.firebaseapp.com'
+  : 'https://r3n4nz3x666.github.io/ProjetoEstoquefirebase/'
 
 /**
  * novoUsuario.
